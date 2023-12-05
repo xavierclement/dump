7e55f7b31d535154e34c26a4966cd289f4929a0f
XAVIER Clément Antoine <clement-antoine.xavier@epitech.eu>
2023-12-05 18:02:27 +0400

    Convert dump for debian

--- README.md
+++ README.md
@@ -1,27 +1,3 @@
-# Installation procedure
+# Dump
 
-## Windows 10 Installation
-
-[Classic installation](https://www.howtogeek.com/197559/how-to-install-windows-10-on-your-pc/) of a [Windows 10 ISO](https://www.microsoft.com/en-us/software-download/windows10) on the machine, don't forget to leave some room on the hard drive for Fedora (minimum 50 GB recommended)
-
-## Fedora 38 Installation
-
-[Installation](https://docs.fedoraproject.org/en-US/fedora/f38/install-guide/install/Preparing_for_Installation/index.html) of [Fedora 36 with selected Desktop](https://archive.fedoraproject.org/pub/fedora/linux/releases/38/Spins/x86_64/iso/)
-
-## Installing dump scripts
-
-Download [installation scripts](https://github.com/Epitech/dump) to `/tmp`
-
-Go to `/tmp` (cd) and `chmod +x install_packages_dump.sh`
-
-Launch `./install_packages_dump.sh` as root user or else `sudo /tmp/install_packages_dump.sh`
-
-# FAQ
-
-### Can I install Fedora first?
-
-Yes, after installing Fedora, perform a [classic installation](https://www.howtogeek.com/197559/how-to-install-windows-10-on-your-pc/) of a [Windows 10 ISO](https://www.microsoft.com/en-us/software-download/windows10) on the machine, make sure that it uses the remaining space and does not delete the Fedora partitions (otherwise, try again).
-
-### After installing Fedora and Windows 10, it starts only on Windows 10, what should you do?
-
-In your BIOS, select the boot order, if a Fedora entry exists, put it first, otherwise the bios will usually suggest creating a custom entry: `\EFI\fedora\grubx64.efi`, don't forget to place this custom entry first in the startup order. This procedure and its necessity depend on the type of computer on which the installation is made.
+The dump is an installetion script that setup your Debian for Epitech course
--- install_packages_dump.sh
+++ install_packages_dump.sh
@@ -2,136 +2,125 @@
 
 clear
 echo "INSTALLING PACKAGES FOR EPITECH'S DUMP"
+
 if [[ $EUID -ne 0 ]]; then
    echo "This script must be run as root" 1>&2
    exit 1
 fi
-cat /etc/fedora-release | grep "Fedora release 38"
-if [[ $? -ne 0 ]]; then
-    echo "This script must be run onto a Fedora 38";
+
+if ! grep -q "Debian" /etc/os-release; then
+    echo "This script must be run onto a Debian-based system";
     exit 1
 fi
 echo "Press ENTER to continue..."
 read
 
-dnf upgrade -y
+apt update
+apt upgrade -y
 
-packages_list=(boost-devel.x86_64
-               boost-static.x86_64
-               ca-certificates.noarch
-               clang.x86_64
-               cmake.x86_64
-               CUnit-devel.x86_64
-               curl.x86_64
-               flac-devel.x86_64
-               freetype-devel.x86_64
-               gcc.x86_64
-               gcc-c++.x86_64
-               gdb.x86_64
+packages_list=(build-essential
+               libboost-dev
+               libboost-all-dev
+               ca-certificates
+               clang
+               cmake
+               libcunit1-dev
+               curl
+               libflac-dev
+               libfreetype6-dev
+               gcc
+               g++
+               gdb
                git
-               glibc.x86_64
-               glibc-devel.x86_64
-               glibc-locale-source.x86_64
-               gmp-devel.x86_64
-               ksh.x86_64
-               elfutils-libelf-devel.x86_64
-               libjpeg-turbo-devel.x86_64
-               libvorbis-devel.x86_64
-               SDL2.x86_64
-               SDL2-static.x86_64
-               SDL2-devel.x86_64
-               libX11-devel.x86_64
-               libXext-devel.x86_64
-               ltrace.x86_64
-               make.x86_64
-               nasm.x86_64
-               ncurses.x86_64
-               ncurses-devel.x86_64
-               ncurses-libs.x86_64
-               net-tools.x86_64
-               openal-soft-devel.x86_64
-               python3-numpy.x86_64
-               python3.x86_64
-               rlwrap.x86_64
-               ruby.x86_64
-               strace.x86_64
-               tar.x86_64
-               tcsh.x86_64
-               tmux.x86_64
-               sudo.x86_64
-               tree.x86_64
-               unzip.x86_64
-               valgrind.x86_64
+               libc6
+               libc6-dev
+               libc6-source
+               libgmp-dev
+               ksh
+               libelf-dev
+               libjpeg62-turbo-dev
+               libvorbis-dev
+               libsdl2-dev
+               libx11-dev
+               libxext-dev
+               ltrace
+               make
+               nasm
+               ncurses
+               libncurses5-dev
+               net-tools
+               libopenal-dev
+               python3-numpy
+               python3
+               rlwrap
+               ruby
+               strace
+               tar
+               tcsh
+               tmux
+               sudo
+               tree
+               unzip
+               valgrind
                vim
                emacs-nox
-               which.x86_64
-               xcb-util-image.x86_64
-               xcb-util-image-devel.x86_64
-               zip.x86_64
-               zsh.x86_64
-               avr-gcc.x86_64
-               qt-devel
-               docker
+               procps
+               libxcb-util0
+               libxcb-util0-dev
+               zip
+               zsh
+               gcc-avr
+               qtbase5-dev
+               docker.io
                docker-compose
-               java-17-openjdk
-               java-17-openjdk-devel
-               boost
-               boost-math
-               boost-graph
+               openjdk-17-jdk
+               libboost-program-options-dev
+               libboost-math-dev
+               libboost-graph-dev
                autoconf
                automake
                tcpdump
                wireshark
                nodejs
-               emacs-tuareg
-               libvirt
-               libvirt-devel
-               virt-install
+               tuareg-mode
+               libvirt-clients
+               libvirt-daemon-system
+               virtinst
                haskell-platform
                golang
-               systemd-devel
-               libgudev-devel
-               php.x86_64
-               php-devel.x86_64
-               php-bcmath.x86_64
-               php-cli.x86_64
-               php-gd.x86_64
-               php-mbstring.x86_64
-               php-mysqlnd.x86_64
-               php-pdo.x86_64
-               php-pear.noarch
-               php-xml.x86_64
-               php-gettext-gettext.noarch
-               php-phar-io-version.noarch
-               php-theseer-tokenizer.noarch
-               SFML.x86_64
-               SFML-devel.x86_64
-               CSFML.x86_64
-               CSFML-devel.x86_64
-               irrlicht.x86_64
-               irrlicht-devel.x86_64
-               rust.x86_64
-               cargo.x86_64
-               mariadb-server.x86_64
-               x264.x86_64
-               lightspark.x86_64
-               lightspark-mozilla-plugin.x86_64
-               teams.x86_64)
+               libsystemd-dev
+               libgudev-1.0-dev
+               php
+               php-dev
+               php-bcmath
+               php-cli
+               php-gd
+               php-mbstring
+               php-mysql
+               php-pdo
+               php-pear
+               php-xml
+               php-gettext-gettext
+               php-phar-io-version
+               php-theseer-tokenizer
+               libsfml-dev
+               libcsfml-dev
+               libirrlicht-dev
+               rustc
+               cargo
+               mariadb-server
+               libx264-dev
+               lightspark
+               browser-plugin-lightspark
+               teams
+               libcriterion-dev)
 
-dnf -y install ${packages_list[@]}
-
-# Criterion
-curl -sSL "https://github.com/Snaipe/Criterion/releases/download/v2.4.2/criterion-2.4.2-linux-x86_64.tar.xz" -o criterion-2.4.2.tar.xz
-tar xf criterion-2.4.2.tar.xz
-cp -r criterion-2.4.2/* /usr/local/
-echo "/usr/local/lib" > /etc/ld.so.conf.d/usr-local.conf
-ldconfig
-rm -rf criterion-2.4.2.tar.xz criterion-2.4.2/
+apt install -y ${packages_list[@]}
 
 # Gradle
-wget https://services.gradle.org/distributions/gradle-7.2-bin.zip
-mkdir /opt/gradle && unzip -d /opt/gradle gradle-7.2-bin.zip && rm -f gradle-7.2-bin.zip
-echo 'export PATH=$PATH:/opt/gradle/gradle-7.2/bin' >> /etc/profile
+wget https://services.gradle.org/distributions/gradle-8.5-bin.zip
+mkdir /opt/gradle && unzip -d /opt/gradle gradle-8.5-bin.zip && rm -f gradle-8.5-bin.zip
+echo 'export PATH=$PATH:/opt/gradle/gradle-8.5/bin' >> /etc/profile
 
 # Stack
 curl -sSL https://get.haskellstack.org/ | sh
